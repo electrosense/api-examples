f3a5b848b8fc315caa7f35ff6be9e218913e7c70
ascalingi <alessio.scalingi@imdea.org>
2021-11-10 17:28:54 +0100

    update version python 3. Remove hard-coded sensor selection. select sensor from list of the active sensor. fix time axes ticks

--- python/SpectrumCountries/spectrumCountries.py
+++ python/SpectrumCountries/spectrumCountries.py
@@ -15,7 +15,9 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see http://www.gnu.org/licenses/.
 #
-#  Authors : Roberto Calvo <roberto [dot] calvo [at] imdea [dot] org)
+#  Authors : Alessio Scalingi <alessio [dot] scalingi [at] imdea [dot] org)
+#            Roberto Calvo <roberto [dot] calvo [at] imdea [dot] org)
+#
 
 import requests
 import json
@@ -75,8 +77,11 @@ def get_spectrum_data(sensor_id, timeBegin, timeEnd, freq_min, freq_max, aggTime
     else:
         print("Response: %d" % (r.status_code))
         return None
-# Prima cosa fatta, selezionare i sensori dall' utente invece di selezionare i sensori coding,
-# aggiungendo la logica per selezionare solamente i sensori che sono "sensing"
+# Scritto il codicde nella versione di python 3.
+#Prima cosa---- selezionare i sensori dall' utente invece di selezionare i sensori coding,
+# aggiungendo la logica per selezionare solamente i sensori che sono "sensing" invece di selezionare i sensory hard coded.
+# fix labels on the y axes accordig to the time
+
 
 
 # Main
@@ -94,19 +99,19 @@ for sensor in sensing_sensors_list:
     count_active_sensor += 1
 
 
-# todo, select two sensors by the user
-
 print(" \n Select two sensors to explore the spectrum \n")
+sensor1 = int(input("Enter first sensor to compare"))  # 28
+sensor2 = int(input("Enter second sensor to compare"))  # 60
 
-sensor_id_A, sensor_id_B = sensing_sensors_list[28]['serial'],sensing_sensors_list[60]['serial']
-sensor_name_A, sensor_name_B = sensing_sensors_list[28]['name'],sensing_sensors_list[60]['name']
-sensor_country_A, sensor_country_B = sensing_sensors_list[28]['country'],sensing_sensors_list[60]['country']
+
+sensor_id_A, sensor_id_B = sensing_sensors_list[sensor1]['serial'],sensing_sensors_list[sensor2]['serial']
+sensor_name_A, sensor_name_B = sensing_sensors_list[sensor1]['name'],sensing_sensors_list[sensor2]['name']
+sensor_country_A, sensor_country_B = sensing_sensors_list[sensor1]['country'],sensing_sensors_list[sensor2]['country']
 
 epoch_time = int(time.time())
 epoch_time = 1599300000
 timeBegin = epoch_time - 3600 * 48
 timeEnd = epoch_time - 3600 * 45
-
 
 sensors_id = [sensor_id_A, sensor_id_B]
 sensors_loc = [sensor_country_A, sensor_country_B]
@@ -119,20 +124,24 @@ freqMin = int(25.00 * 1e6)
 freqMax = int(1600.00 * 1e6)
 
 fig, ax = plt.subplots(2, 1)
-plt.subplots_adjust(top=0.6)
+plt.suptitle('Spectrum usage (' + time.strftime("%d %b %Y", time.gmtime(timeBegin))+')',y=1.0)
 response = get_spectrum_data(sensor_id_A, timeBegin, timeEnd, freqMin, freqMax, time_resolution, freq_resolution)
-date_values = np.arange(timeBegin, timeEnd, (timeEnd - timeBegin) / 10)
-date_text = np.array([np.array(time.strftime("%H:%M", time.gmtime(xi))) for xi in date_values])
+
 if (response != None):
      A = np.array([np.array(xi) for xi in response['values']])
      ax[0].imshow(A, aspect='auto', cmap=cm.jet)
      img = ax[0].imshow(A, aspect='auto',
                 extent=[freqMin / 1e6, freqMax / 1e6, timeEnd, timeBegin],
                 cmap=cm.jet)
-     date_text = np.array([np.array(time.strftime("%d/%m/%y %H:%M", time.gmtime(xi))) for xi in date_values])
-     ax[0].set_yticklabels(date_text)
-     ax[0].set_title("Sensor %s" % sensor_name_A,pad=4)
-     ax[0].set_xlabel('frequency (MHz)')
+
+     ylabels = [item.get_text() for item in ax[0].get_yticklabels()]
+     date_values = np.arange(timeBegin, timeEnd, (timeEnd - timeBegin) / len(ylabels))
+     date_text = [(time.strftime("%H:%M", time.gmtime(xi))) for xi in date_values]
+     date_text
+     ylabels = date_text
+     ax[0].set_yticklabels(ylabels)
+     ax[0].set_title("Sensor %s" % sensor_name_A,pad=2)
+     # ax[0].set_xlabel('frequency (MHz)')
      ax[0].set_ylabel('time')
      c = plt.colorbar(img, ax=ax[0])
      c.set_label("Power (dB)")
@@ -147,17 +156,20 @@ if (response != None):
      img = ax[1].imshow(A, aspect='auto',
                 extent=[freqMin / 1e6, freqMax / 1e6, timeEnd, timeBegin],
                 cmap=cm.jet)
-     date_text = np.array([np.array(time.strftime("%d/%m/%y %H:%M", time.gmtime(xi))) for xi in date_values])
-     ax[1].set_yticklabels(date_text)
-     ax[1].set_title("Sensor %s" % sensor_name_B,pad=4)
+     ylabels = [item.get_text() for item in ax[0].get_yticklabels()]
+     date_values = np.arange(timeBegin, timeEnd, (timeEnd - timeBegin) / len(ylabels))
+     date_text = [(time.strftime("%H:%M", time.gmtime(xi))) for xi in date_values]
+     date_text
+     ylabels = date_text
+     ax[1].set_yticklabels(ylabels)
+     ax[1].set_title("Sensor %s" % sensor_name_B,pad=2)
      ax[1].set_xlabel('frequency (MHz)')
      ax[1].set_ylabel('time')
      c = plt.colorbar(img, ax=ax[1])
      c.set_label("Power (dB)")
 
-fig.suptitle('Spectrum usage (' + time.strftime("%d %b %Y", time.gmtime(timeBegin))+')',y=0.99)
 fig.tight_layout()
-plt.savefig('./resources/output.png')
+#plt.savefig('./resources/output.png')
 plt.show()
     
 
